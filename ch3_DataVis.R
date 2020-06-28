d4d62822fb88c6cc6674f2851a645e8725de92b8
Jason A. Belsky (JAB-YOGA-900) <jabelsky@gmail.com>
2020-06-28 11:54:43 -0400

    Created first ggplot2 and added info on plot structure

--- ch3_DataVis.R
+++ ch3_DataVis.R
@@ -11,3 +11,17 @@ if(! "tidyverse" %in% rownames(installed.packages())){
 
 # Load in tidyverse
 library("tidyverse")
+
+# mpg is a sample dataframe in ggplot2 that lists US EPA metrics for 38 cars
+
+# Create a plot between engine size (displ) and highway gas mileage (hwy)
+# Note this will not display a graph if sourced
+ggplot(data = mpg) + geom_point(mapping = aes(x = displ, y = hwy))
+
+# Follows the template:
+# ggplot(data = <DATA>) + <GEOM_FUNCTION>(mapping = aes(<MAPPINGS>))
+# ggplot() creates coordinate system that can have layers added
+# geom_point() is a layer for creating a scatter plot
+#  within each layer, there is a mapping argument that defines how variables in the dataset are mapped to visual properties
+#  requires aes() method (for 'Aesthetic'), the specifies which variables within the ggplot data should be used to map to the x and y axes
+#    Aesthetic mapping describe how variables in the data are mapped to visual properties (aesthetics) of geoms
